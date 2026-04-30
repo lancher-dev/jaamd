bc7c65c76114f4324aee2d7eb4a595c29c161987
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-04-30 13:31:39 +0200

    fix: enhance mode switch in example

--- example/astro.config.mjs
+++ example/astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 import jaamd from "jaamd";
 
 export default defineConfig({
-  integrations: [jaamd()],
+  integrations: [jaamd({ theme: { light: "github-light", dark: "one-dark-pro" } })],
 });
