189116aaa94c465b1030b9c2c558c040d31f6182
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-08-14 23:34:59 +0200

    fix: escape apostrophes in code tab labels

--- src/plugins/remark-code-tabs.ts
+++ src/plugins/remark-code-tabs.ts
@@ -7,7 +7,8 @@ function escapeHtml(value: string): string {
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 }
 
 /**
